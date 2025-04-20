f69cd8a8759d195f6d04a1581dd69bedee688325
kater <ari@Ariranha.>
2025-04-20 20:10:30 -0300

    agora parece que deu certo finalmente

--- P4 - Escalonador por Prioridades/ppos_core.c
+++ P4 - Escalonador por Prioridades/ppos_core.c
@@ -14,6 +14,10 @@ GRR: 20190366
 #include "ppos_data.h"
 #include "queue.h"
 
+#define READY 0
+#define RUNNING 1
+#define SUSPENDED 2
+
 #define STACKSIZE 32*1024
 int task_id_counter = 0;
 task_t main_task;
@@ -24,6 +28,8 @@ ucontext_t main_context;
 queue_t *task_queue;
 
 task_t *scheduler(){
+    // escalonador de tarefas
+    // retorna a próxima tarefa a ser executada
     #ifdef DEBUG
         printf ("scheduler: entrando\n") ;
     #endif
@@ -33,13 +39,13 @@ task_t *scheduler(){
         #endif
         return NULL;
     }
-    queue_t *cur_q = task_queue;
+    queue_t *cur_q = task_queue; // ponteiro para o nodo atual da fila
     task_t *cur_task; // ponteiro para a tarefa atual
     task_t *next = (task_t*)cur_q; // ponteiro para a próxima tarefa
 
     do{
         cur_task = (task_t *) cur_q;
-        if (cur_task->prio_dinamica <= next->prio_dinamica){
+        if (cur_task->prio_dinamica < next->prio_dinamica){
             next = cur_task;
             // cur_task = next;
         }
@@ -49,20 +55,20 @@ task_t *scheduler(){
     cur_q = task_queue;
     do {
         cur_task = (task_t*) cur_q;
-        if (cur_task != next)
+        if (cur_task != next && cur_task->prio_dinamica > -20){
             cur_task->prio_dinamica--;
         // if (next->prio_dinamica > -20){
         //     (next->prio_dinamica)--;
-        // }
+        }
         cur_q = cur_q->next;
     } while (cur_q != task_queue);
-    cur_task->prio_dinamica = cur_task->prio_estatica;
-    queue_remove((queue_t **) &task_queue, (queue_t *) next);
+    next->prio_dinamica = next->prio_estatica;
+    queue_remove((queue_t **) &task_queue, (queue_t *) next); // remove a tarefa da fila 
     #ifdef DEBUG
         printf ("scheduler: tarefa %d\n", next->id) ;
         printf ("scheduler: %d tarefas na fila\n", queue_size(task_queue));
     #endif
-    return cur_task;
+    return next;
 }
 
 void task_setprio (task_t *task, int prio){
@@ -92,44 +98,52 @@ int task_getprio (task_t *task){
 }
 
 void dispatcher(){
+    // responsavel por executar as tarefas uma após a outra
     // queue_remove(&task_queue, (queue_t *) dispatcher_task);
     #ifdef DEBUG
         printf ("dispatcher: entrando\n") ;
     #endif
-    task_t *next;
+    task_t *next; // ponteiro para a próxima tarefa
     while (queue_size(task_queue) > 0){
         #ifdef DEBUG
             printf ("dispatcher: %d tarefas na fila\n", queue_size(task_queue));
         #endif
-        next = scheduler();
+        next = scheduler(); // chama o escalonador para pegar a próxima tarefa
         if (next){
-            // next->status = 1; // status = 1 significa que a tarefa está rodando
-            task_switch(next);
+            next->status = RUNNING; 
+            task_switch(next); // troca o contexto da tarefa atual pelo da próxima tarefa
             // queue_append(&task_queue, (queue_t*) next);
-            // switch (next->status){
-            //     case 0: // tarefa pronta
-            //         break;
-            //     case 1: // tarefa rodando
-            //         break;
-            //     default:
-            //         break;
-            // }
+            switch (next->status){
+                case READY: // tarefa pronta
+                    break;
+                case RUNNING: // tarefa rodando
+                    next->status = READY; // muda o status da tarefa para pronta
+                    queue_append((queue_t **)&task_queue, (queue_t*) next); // adiciona a tarefa na fila de tarefas
+                    break;
+                default:
+                    break;
+            }
         }
     }
-    task_exit(0);
+    task_exit(0); // encerra o dispatcher
     #ifdef DEBUG
         printf ("dispatcher: saindo\n") ;
     #endif
 }
 
 void task_yield (){
+    // troca o contexto da tarefa atual pelo do dispatcher
     #ifdef DEBUG
         printf ("task_yield: entrando\n") ;
     #endif
-    queue_remove((queue_t **)&task_queue, (queue_t*) current_task);
-    queue_append((queue_t **)&task_queue, (queue_t*) current_task);
-    current_task->status = 0;
-    task_switch(&dispatcher_task);
+    // se a tarefa atual for o dispatcher, não faz nada
+    if (current_task != &dispatcher_task){ 
+        // remove a tarefa atual da fila de tarefas e adiciona novamente
+        current_task->status = READY; // muda o status da tarefa para pronta
+        // queue_remove((queue_t **)&task_queue, (queue_t*) current_task);
+        queue_append((queue_t **)&task_queue, (queue_t*) current_task);
+    }
+    task_switch(&dispatcher_task); // troca o contexto da tarefa atual pelo do dispatcher
     #ifdef DEBUG
         printf ("task_yield: saindo\n") ;
     #endif
@@ -139,17 +153,23 @@ void ppos_init (){
     #ifdef DEBUG
         printf ("ppos_init: entrando\n") ;
     #endif
-    setvbuf (stdout, 0, _IONBF, 0) ;
-    
+    setvbuf (stdout, 0, _IONBF, 0) ; // desabilita o buffer da saída padrão
+    task_queue = NULL; // inicializa a fila de tarefas
     current_task = &main_task;
     #ifdef DEBUG
         printf ("criando tarefa: dispatcher\n") ;
     #endif
-    task_init(&dispatcher_task, dispatcher, NULL);
+    task_init(&dispatcher_task, dispatcher, NULL); // cria a tarefa dispatcher
     main_task.id = task_id_counter++;
-    main_task.status = 0; // status = 0 significa que a tarefa está pronta 
+    main_task.status = READY;
+    main_task.prio_dinamica = 0; // prioridade padrão
+    main_task.prio_estatica = 0; // prioridade padrão
+    main_task.prev = NULL;
+    main_task.next = NULL;
+    main_task.context = main_context; // inicializa o contexto da tarefa main
+
     queue_append((queue_t **)&task_queue, (queue_t*) &main_task);
-    task_switch(&dispatcher_task);
+    task_switch(&dispatcher_task); // transfere o controle para o dispatcher
     #ifdef DEBUG
         printf ("ppos_init: saindo\n") ;
     #endif
@@ -162,9 +182,9 @@ int task_init (task_t *task, void (*start_routine)(void *),  void *arg){
     #endif
     // ucontext_t new_context;
 
-    getcontext (&task->context) ;
+    getcontext (&task->context) ; // pega o contexto atual da tarefa
 
-    char *stack = malloc (STACKSIZE) ;
+    char *stack = malloc (STACKSIZE) ; // aloca a pilha para a tarefa
     if (stack)
     {
         task->context.uc_stack.ss_sp = stack ;
@@ -179,17 +199,17 @@ int task_init (task_t *task, void (*start_routine)(void *),  void *arg){
     }
  
     
-    makecontext (&task->context, (void*)start_routine, 1, arg) ;
+    makecontext (&task->context, (void*)start_routine, 1, arg) ; // cria o contexto da tarefa
     
     // task->context = new_context;
-    task->status = 0; // status = 0 significa que a tarefa está pronta
+    task->status = READY; // status = 0 significa que a tarefa está pronta
     task->prio_dinamica = 0; // prioridade padrão
     task->prio_estatica = 0; // prioridade padrão
     task->prev = NULL;
     task->next = NULL;
     task->id = task_id_counter++;
 
-    if (task != &dispatcher_task) {
+    if (task != &dispatcher_task) { // não adiciona o dispatcher na fila de tarefas
         queue_append((queue_t **)&task_queue, (queue_t*) task);
     }
 
@@ -204,9 +224,9 @@ int task_switch (task_t *task){
     #ifdef DEBUG
         printf ("task_switch: entrando\n") ;
     #endif
-    task_t *prev_task = current_task;
-    current_task = task;
-    swapcontext (&(prev_task->context), &(current_task->context)) ;
+    task_t *prev_task = current_task; // ponteiro para a tarefa atual
+    current_task = task; // atualiza a tarefa atual
+    swapcontext (&(prev_task->context), &(current_task->context)) ; // troca o contexto da tarefa atual pelo da próxima tarefa
     #ifdef DEBUG
         printf ("task_switch: saindo\n") ;
     #endif
@@ -223,10 +243,12 @@ void task_exit (int exit_code){
     // }
     if (current_task == &dispatcher_task) {
         // exit(exit_code);
+        task_switch(&main_task); // transfere o controle para a tarefa main
         exit_code = exit_code;
     } else {
-        current_task->status = 2; 
-        task_switch(&dispatcher_task);
+        current_task->status = SUSPENDED;
+        // queue_remove((queue_t **)&task_queue, (queue_t*) current_task); // remove a tarefa da fila
+        task_switch(&dispatcher_task); // ttransfere o controle para o dispatcher
     }
     // exit(exit_code);
     // free(current_task->context.uc_stack.ss_sp);
